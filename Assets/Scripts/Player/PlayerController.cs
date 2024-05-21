36e123c8192ceccc6f40d94ecf144c77e8a870e8
DeivasigamaniSelvam <dsmeighteen@gmail.com>
2024-05-21 17:16:33 +0530

    camera control

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,210 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public GameObject BodyPrefab;
+    public int Gap = 10;
+    // Lists
+    private List<GameObject> BodyParts = new List<GameObject>();
+    private List<Vector3> PositionsHistory = new List<Vector3>();
+    private CharacterController controller;
+    private Vector3 move;
+    public float forwardSpeed;
+    public float maxSpeed;
+
+    private int desiredLane = 1;//0:left, 1:middle, 2:right
+    public float laneDistance = 2.5f;//The distance between tow lanes
+
+    public bool isGrounded;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+
+    public float gravity = -12f;
+    public float jumpHeight = 2;
+    private Vector3 velocity;
+
+    public Animator animator;
+    private bool isSliding = false;
+
+    public float slideDuration = 1.5f;
+
+    bool toggle = false;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        Time.timeScale = 1.2f;
+    }
+    private void GrowSnake()
+    {
+        // Instantiate body instance and
+        // add it to the list
+        //GameObject body = Instantiate(BodyPrefab);
+        //BodyParts.Add(body);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!PlayerManager.isGameStarted || PlayerManager.gameOver)
+            return;
+
+        //Increase Speed
+        if (toggle)
+        {
+            toggle = false;
+            if (forwardSpeed < maxSpeed)
+                forwardSpeed += 0.1f * Time.fixedDeltaTime;
+        }
+        else
+        {
+            toggle = true;
+            if (Time.timeScale < 2f)
+                Time.timeScale += 0.005f * Time.fixedDeltaTime;
+        }
+    }
+
+    void Update()
+    {
+        //PositionsHistory.Insert(0, transform.position);
+        //// Move body parts
+        //int index = 0;
+        //foreach (var body in BodyParts)
+        //{
+        //    Vector3 point = PositionsHistory[Mathf.Clamp(index * Gap, 0, PositionsHistory.Count - 1)];
+
+        //    // Move body towards the point along the snakes path
+        //    Vector3 moveDirection = point - body.transform.position;
+        //    body.transform.position += moveDirection * (forwardSpeed+3) * Time.deltaTime;
+
+        //    // Rotate body towards the point along the snakes path
+        //    body.transform.LookAt(point);
+
+        //    index++;
+        //}
+        if (!PlayerManager.isGameStarted || PlayerManager.gameOver)
+            return;
+
+        //animator.SetBool("isGameStarted", true);
+        move.z = forwardSpeed;
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.17f, groundLayer);
+        //animator.SetBool("isGrounded", isGrounded);
+        if (isGrounded && velocity.y < 0)
+            velocity.y = -1f;
+
+        if (isGrounded)
+        {
+            if (SwipeManager.swipeUp)
+                Jump();
+
+            if (SwipeManager.swipeDown && !isSliding)
+                StartCoroutine(Slide());
+        }
+        else
+        {
+            velocity.y += gravity * Time.deltaTime;
+            if (SwipeManager.swipeDown && !isSliding)
+            {
+                StartCoroutine(Slide());
+                velocity.y = -10;
+            }                
+
+        }
+        controller.Move(velocity * Time.deltaTime);
+
+        //Gather the inputs on which lane we should be
+        if (SwipeManager.swipeRight)
+        {
+            desiredLane++;
+            if (desiredLane == 3)
+                desiredLane = 2;
+        }
+        if (SwipeManager.swipeLeft)
+        {
+            desiredLane--;
+            if (desiredLane == -1)
+                desiredLane = 0;
+        }
+
+        //Calculate where we should be in the future
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (desiredLane == 0)
+            targetPosition += Vector3.left * laneDistance;
+        else if (desiredLane == 2)
+            targetPosition += Vector3.right * laneDistance;
+
+        //transform.position = targetPosition;
+        if (transform.position != targetPosition)
+        {
+            Vector3 diff = targetPosition - transform.position;
+            Vector3 moveDir = diff.normalized * 30 * Time.deltaTime;
+            if (moveDir.sqrMagnitude < diff.magnitude)
+                controller.Move(moveDir);
+            else
+                controller.Move(diff);
+        }
+
+        controller.Move(move * Time.deltaTime);
+    }
+
+    private void Jump()
+    {   
+        StopCoroutine(Slide());
+        //animator.SetBool("isSliding", false);
+        animator.SetTrigger("jump");
+
+        animator.SetTrigger("run");
+        controller.center = Vector3.zero;
+        controller.height = 2;
+        isSliding = false;
+   
+        velocity.y = Mathf.Sqrt(jumpHeight * 2 * -gravity);
+    }
+
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if(hit.transform.tag == "Obstacle")
+        {
+            animator.SetTrigger("loss");
+            PlayerManager.gameOver = true;
+            PlayerManager.instance.PlayAudiobyIndex(0);
+            //FindObjectOfType<AudioManager>().PlaySound("GameOver");
+        }
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "child")
+        {
+            Debug.LogError("hello");
+            PlayerManager.instance.PlayAudiobyIndex(3);
+            //GrowSnake();
+            other.gameObject.SetActive(false);
+            //PlayerManager.gameOver = true;
+            //FindObjectOfType<AudioManager>().PlaySound("GameOver");
+        }
+    }
+
+    private IEnumerator Slide()
+    {
+        isSliding = true;
+        animator.SetTrigger("roll");
+        animator.SetTrigger("run");
+        yield return new WaitForSeconds(0.25f/ Time.timeScale);
+        controller.center = new Vector3(0, -0.5f, 0);
+        controller.height = 1;
+
+        yield return new WaitForSeconds((slideDuration - 0.25f)/Time.timeScale);
+
+        //animator.SetBool("isSliding", false);
+
+        controller.center = Vector3.zero;
+        controller.height = 2;
+
+        isSliding = false;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Animator animator;
+    public static PlayerManager instance;
+    public AudioSource audiosource;
+    public AudioClip[] audioclip;
+    public static bool gameOver;
+    public GameObject gameOverPanel, ingameui;
+
+    public static bool isGameStarted;
+
+    public GameObject startbutton, Levelcompleted;
+    public static int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gemsText;
+    public TextMeshProUGUI newRecordText;
+    public TextMeshProUGUI LevelcompText;
+
+    public static bool isGamePaused;
+    //public GameObject[] characterPrefabs;
+
+
+    private void Awake()
+    {
+        instance = this;
+        //int index = PlayerPrefs.GetInt("SelectedCharacter");
+        //GameObject go = Instantiate(characterPrefabs[index], transform.position, Quaternion.identity);
+    }
+
+
+    public void run()
+    {
+        
+    }
+
+    public void PlayAudiobyIndex(int index)
+    {
+        audiosource.clip = audioclip[index];
+        audiosource.Play();
+    }
+
+    public void restartlevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void start()
+    {
+        isGameStarted = true;
+    }
+
+    void Start()
+    {
+        //Invoke("run", 1.0f);
+        score = 0;
+        Time.timeScale = 1;
+        gameOver = isGameStarted = isGamePaused= false;
+        if(PlayerPrefs.HasKey("levelcompletedingame"))
+        {
+            level =  PlayerPrefs.GetInt("levelcompletedingame", level);
+            LevelcompText.text = "Level: "+level.ToString();
+        }
+        else
+        {
+            LevelcompText.text = "Level: " + level.ToString();
+        }
+    }
+
+    void Update()
+    {
+        //Update UI
+        gemsText.text = "Coins: "+PlayerPrefs.GetInt("TotalGems", 0).ToString();
+        scoreText.text = "Score: :"+score.ToString();
+        if (score > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            newRecordText.text = "HiScore: " + score;
+            PlayerPrefs.SetInt("HighScore", score);
+        }
+        //Game Over
+        if (gameOver)
+        {
+            //Time.timeScale = 0;
+            
+            
+            gameOverPanel.SetActive(true);
+            //Destroy(gameObject);
+        }
+
+        //Start Game
+        if (SwipeManager.tap  && !isGameStarted)
+        {
+            animator.SetTrigger("run");
+            startbutton.SetActive(false);
+            ingameui.SetActive(true);
+            isGameStarted = true;
+            StartCoroutine(time());
+        }
+    }
+
+    int timer,level=1;
+    int[] timedetails = new int[10] {10,20,30,40,50,80,90,100,140,1500};
+    IEnumerator time()
+    {
+    while (true && timer < timedetails[level-1])
+    {
+        timeCount();
+        yield return new WaitForSeconds(1);
+    }
+       
+    }
+    void timeCount()
+    {
+        timer += 1;
+        Debug.LogError(timer);
+        Debug.LogError(timer >= timedetails[level - 1]);
+        if (timer >= timedetails[level - 1])
+        {
+            Levelcompleted.SetActive(true);
+            animator.SetTrigger("win");
+            level++;
+            Debug.LogError("l "+level);
+            LevelcompText.text = "Level: " + level.ToString();
+            PlayerPrefs.SetInt("levelcompletedingame", level);
+            StopCoroutine(time());
+            if (!isGamePaused && !gameOver)
+            {
+                //Time.timeScale = 0;
+                isGamePaused = true;
+            }
+            PlayAudiobyIndex(1);
+        }
+    }
+
+
+}
